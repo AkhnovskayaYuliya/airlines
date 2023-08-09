106aaa9214e9483bebed20ff9f1f0225792ae665
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-08-09 15:18:12 +0300

    changed

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 21589;
+        int mile = 20;
+        int bonus = cost / mile;
+        System.out.println(bonus);
+
+
+    }
+}
