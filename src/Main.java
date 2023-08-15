e0b17da94732a7ad7d67c2a6503dd4ad7165240d
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-08-15 15:52:16 +0300

    changeed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
- public class Main {
+public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 96000;
-        int miles = service.calculate(price); //
+        int miles = service.calculate(price);
+
         System.out.println(miles);
 
     }
